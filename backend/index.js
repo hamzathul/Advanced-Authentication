608e362bd24040b8aafbb276f3faad995a4553f0
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-12 12:06:22 +0530

    func: signup

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,13 @@ import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use(express.json()) // allows to parse incoming requests : req.body
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
