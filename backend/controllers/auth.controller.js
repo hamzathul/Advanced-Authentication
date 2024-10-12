08c6de1acb64c14a7760f91fe73da734e4c998ae
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-12 09:07:18 +0530

    model setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-export const signup = async(req, res)=>{
-    res.send("signup route")
-}
+export const signup = async (req, res) => {
+  res.send("signup route");
+};
 
 export const login = async (req, res) => {
   res.send("login route");
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
-import authRoutes from './routes/auth.route.js'
+import authRoutes from "./routes/auth.route.js";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(3000, ()=>{
-    connectDB()
-    console.log("Server is running on port 3000")
-})
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on port 3000");
+});
