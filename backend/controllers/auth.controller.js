95b6e1e8952c3f04204221623c404e2791baf9c9
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-13 11:10:26 +0530

    add: logout endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,5 +83,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("logout route");
+  res.clearCookie("token")
+  res.status(200).json({success:true, message:"Logout successfully"})
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/signup", logout);
+router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
 
 export default router;
