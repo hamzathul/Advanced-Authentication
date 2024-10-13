0dbea141b91c7da44b9de7e802202e71278233e8
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-13 08:40:29 +0530

    start: mailtrap integration

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,27 @@
+import { MailtrapClient } from "mailtrap";
+
+const TOKEN = "4d3a307650c5154e70ae8d7a7f987ffa";
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "ehamzathulfavas@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
