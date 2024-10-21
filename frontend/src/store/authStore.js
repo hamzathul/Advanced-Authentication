334b3394f2d119320f783ad6c030f22fecf191f9
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-21 11:10:38 +0530

    feat: reset password function

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -12,6 +12,7 @@ export const useAuthStore = create((set) => ({
   error: null,
   isLoading: false,
   isCheckingAuth: true,
+  message:null,
 
   signup: async (email, password, name) => {
     // signup function
@@ -113,5 +114,19 @@ export const useAuthStore = create((set) => ({
       set({isLoading:false, error:error.response.data.message||"Error sending reset password email"})
       throw error
     }
+  },
+
+  resetPassword:async(token, password)=>{
+    set({isLoading:true, error:null})
+    try {
+    const response = await axios.post(`${API_URL}/reset-password/${token}`, {
+      password,
+      token
+    });
+      set({message:response.data.message, isLoading:false})
+    } catch (error) {
+      set({isLoading:false, error:error.response.data.message||"Error resetting password"})
+      throw error
+    }    
   }
 }));
